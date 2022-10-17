76b193548e5f0afec9b741538c49fd90857d8dc7
Bryan Elliott <bryan_elliott@comcast.com>
2022-10-17 14:04:02 -0400

    Restored old difficulty weights

--- docs/difficulty.mjs
+++ docs/difficulty.mjs
@@ -3,7 +3,7 @@ import COLORS from "./COLORS.mjs";
 const params = [];
 
 export const difficulty = ({ cap, col }) => 
-  (Math.pow(col, 0.5) * Math.pow(cap, 1.25)) * 100 / 81;
+  Math.pow(col, 0.6) * Math.pow(cap, 0.4);
 
 for (let col = 0; col <= (COLORS.length - 3); col++) {
   for (let cap = 0; cap <= 7; cap++) {
